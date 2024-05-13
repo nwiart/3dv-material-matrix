1e4f32af8dd9ef0a4adefe25fba2856e976f1b5a
nwiart <nwiart@gaming.tech>
2024-05-13 09:51:33 +0200

    Moved API calls to another file

---
+++ scripts/api_calls.js
@@ -0,0 +1,47 @@
+import {
+	get,
+	post,
+	del,
+	put
+} from "./request.js";
+
+
+export async function listUsers() {
+	return await get(
+		"/app/v1/users",
+		{"api_key": apiToken});
+}
+
+export async function registerUser(name) {
+	return await post(
+		"/app/v1/users",
+		{"Content-type": "application/json", "api_key": apiToken},
+		{"username": name});
+}
+
+export async function generateUserToken(user_id, ttl) {
+	return await post(
+		`/app/v1/users/${user_id}/tokens`,
+		{"Content-type": "application/json", "api_key": apiToken},
+		{"scope": "manage", "ttl": ttl});
+}
+
+export async function deleteUser(user_id) {
+	return await del(
+		`/app/v1/users/${user_id}`,
+		{"api_key": apiToken});
+}
+
+export async function grantAccessToFolder(folder_id, member_type, member_id, access) {
+	return await put(
+		`/app/v1/folders/${folder_id}/access/${member_type}/${member_id}`,
+		{"Content-type": "application/json", "api_key": apiToken},
+		{"access": access});
+}
+
+export async function createAsset(folder_id, asset_type, asset_name) {
+	return await post(
+		`/app/v1/folders/${folder_id}/assets`,
+		{"Content-type": "application/json", "api_key": apiToken},
+		{"asset_type": asset_type, "name": asset_name});
+}
--- scripts/main.js
+++ scripts/main.js
@@ -7,52 +7,15 @@ import {
 } from "../config.js";
 
 import {
-	get,
-	post,
-	del,
-	put
-} from "./request.js";
+	listUsers,
+	registerUser,
+	generateUserToken,
+	grantAccessToFolder,
+	createAsset
+} from "./api_calls.js";
 
 
-async function listUsers() {
-	return await get(
-		"/app/v1/users",
-		{"api_key": apiToken});
-}
 
-async function registerUser(name) {
-	return await post(
-		"/app/v1/users",
-		{"Content-type": "application/json", "api_key": apiToken},
-		{"username": name});
-}
-
-async function generateUserToken(user_id, ttl) {
-	return await post(
-		`/app/v1/users/${user_id}/tokens`,
-		{"Content-type": "application/json", "api_key": apiToken},
-		{"scope": "manage", "ttl": ttl});
-}
-
-async function deleteUser(user_id) {
-	return await del(
-		`/app/v1/users/${user_id}`,
-		{"api_key": apiToken});
-}
-
-async function grantAccessToFolder(folder_id, member_type, member_id, access) {
-	return await put(
-		`/app/v1/folders/${folder_id}/access/${member_type}/${member_id}`,
-		{"Content-type": "application/json", "api_key": apiToken},
-		{"access": access});
-}
-
-async function createAsset(folder_id, asset_type, asset_name) {
-	return await post(
-		`/app/v1/folders/${folder_id}/assets`,
-		{"Content-type": "application/json", "api_key": apiToken},
-		{"asset_type": asset_type, "name": asset_name});
-}
 
 
 // List of all four shaders (standard, sheen, anisotropic, clearcoat) in their textured and non-textured variants.
